2ea908969b39b83327e50e8a02798e29efb31675
Michal Maciejewski <michal.mac@gmail.com>
2020-08-07 23:41:38 +0200

    extract AbstractRebalancingParams from MinCostFlowRebalancingParams

--- contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/rebalancing/mincostflow/MinCostFlowRebalancingParams.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/rebalancing/mincostflow/MinCostFlowRebalancingParams.java
@@ -18,47 +18,23 @@
 
 package org.matsim.contrib.drt.optimizer.rebalancing.mincostflow;
 
-import java.net.URL;
 import java.util.Map;
 
-import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
-import org.matsim.contrib.drt.analysis.zonal.ZonalDemandAggregator;
+import org.matsim.contrib.drt.optimizer.rebalancing.AbstractRebalancingParams;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigGroup;
-import org.matsim.core.config.ReflectiveConfigGroup;
-
-import com.google.common.base.Verify;
 
 /**
  * @author michalm
  */
-public final class MinCostFlowRebalancingParams extends ReflectiveConfigGroup {
+public final class MinCostFlowRebalancingParams extends AbstractRebalancingParams {
 	public static final String SET_NAME = "minCostFlowRebalancing";
 
-	public enum ZonalDemandAggregatorType {PreviousIteration,
-		TimeDependentActivityBased,
-		EqualVehicleDensity,
-		FirstActivityCount}
-
-	public enum RebalancingZoneGeneration {GridFromNetwork, ShapeFile}
-
-	public static final String INTERVAL = "interval";
-	static final String INTERVAL_EXP = "Specifies how often empty vehicle rebalancing is executed."
-			+ " Must be positive. Default is 1800 s. Expects an Integer Value";
-
-	public static final String MIN_SERVICE_TIME = "minServiceTime";
-	static final String MIN_SERVICE_TIME_EXP = //
-			"Minimum remaining service time of an idle/busy vehicle to be considered as rebalancable/soon-idle (respectively)."
-					+ " Default is 3600 s. In general, should be higher than interval (e.g. 2 x interval).";
-
-	public static final String MAX_TIME_BEFORE_IDLE = "maxTimeBeforeIdle";
-	static final String MAX_TIME_BEFORE_IDLE_EXP = //
-			"Maximum remaining time before busy vehicle becomes idle to be considered as soon-idle vehicle."
-					+ " Default is 900 s. In general should be lower than interval (e.g. 0.5 x interval)";
+	public enum ZonalDemandAggregatorType {
+		PreviousIteration, TimeDependentActivityBased, EqualVehicleDensity, FirstActivityCount
+	}
 
 	public static final String TARGET_ALPHA = "targetAlpha";
 	static final String TARGET_ALPHA_EXP = "alpha coefficient in linear target calculation."
@@ -68,29 +44,8 @@ public final class MinCostFlowRebalancingParams extends ReflectiveConfigGroup {
 	static final String TARGET_BETA_EXP = "beta constant in linear target calculation."
 			+ " In general, should be lower than 1.0 to prevent over-reacting and high empty mileage.";
 
-	public static final String CELL_SIZE = "cellSize";
-	static final String CELL_SIZE_EXP = "size of square cells used for demand aggregation."
-			+ " Depends on demand, supply and network. Often used with values in the range of 500 - 2000 m";
-
 	public static final String ZONAL_DEMAND_AGGREGATOR_TYPE = "zonalDemandAggregatorType";
 	static final String ZONAL_DEMAND_AGGREGATOR_TYPE_EXP = "Defines the methodology for demand estimation. Can be one of either [PreviousIteration, TimeDependentActivityBased, EqualVehicleDensity, FirstActivityCount] Current default is PreviousIteration";
-
-	public static final String REBALANCING_ZONES_GENERATION = "rebalancingZonesGeneration";
-	static final String REBALANCING_ZONES_GENERATION_EXP = "Logic for generation of zones for demand estimation while rebalancing. Value can be GridFromNetwork or ShapeFile Default is GridFromNetwork";
-
-	private static final String REBALANCING_ZONES_SHAPE_FILE = "rebalancingZonesShapeFile";
-	private static final String REBALANCING_ZONES_SHAPE_FILE_EXP = "allows to configure rebalancing zones."
-			+ "Used with rebalancingZonesGeneration=ShapeFile";
-
-
-	@Positive
-	private int interval = 1800;// [s]
-
-	@Positive
-	private double minServiceTime = 2 * interval;// [s]
-
-	@PositiveOrZero
-	private double maxTimeBeforeIdle = 0.5 * interval;// [s], if 0 then soon-idle vehicle will not be considered
 
 	@PositiveOrZero
 	private double targetAlpha = Double.NaN;
@@ -98,17 +53,8 @@ public final class MinCostFlowRebalancingParams extends ReflectiveConfigGroup {
 	@PositiveOrZero
 	private double targetBeta = Double.NaN;
 
-	@Positive
-	private double cellSize = Double.NaN;// [m]
-
 	@NotNull
 	private MinCostFlowRebalancingParams.ZonalDemandAggregatorType zonalDemandAggregatorType = ZonalDemandAggregatorType.PreviousIteration;
-
-	@NotNull
-	private RebalancingZoneGeneration rebalancingZonesGeneration = RebalancingZoneGeneration.GridFromNetwork;
-
-	@Nullable
-	private String rebalancingZonesShapeFile = null;
 
 	public MinCostFlowRebalancingParams() {
 		super(SET_NAME);
@@ -117,83 +63,15 @@ public final class MinCostFlowRebalancingParams extends ReflectiveConfigGroup {
 	@Override
 	protected void checkConsistency(Config config) {
 		super.checkConsistency(config);
-
-		if (getMinServiceTime() <= getMaxTimeBeforeIdle()) {
-			throw new RuntimeException(MinCostFlowRebalancingParams.MIN_SERVICE_TIME
-					+ " must be greater than "
-					+ MinCostFlowRebalancingParams.MAX_TIME_BEFORE_IDLE);
-		}
-
-		Verify.verify(
-				getRebalancingZonesGeneration() != RebalancingZoneGeneration.ShapeFile || getRebalancingZonesShapeFile() != null,
-				REBALANCING_ZONES_SHAPE_FILE
-						+ " must not be null when "
-						+ REBALANCING_ZONES_GENERATION
-						+ " is "
-						+ RebalancingZoneGeneration.ShapeFile);
 	}
 
 	@Override
 	public Map<String, String> getComments() {
 		Map<String, String> map = super.getComments();
-		map.put(INTERVAL, INTERVAL_EXP);
-		map.put(MIN_SERVICE_TIME, MIN_SERVICE_TIME_EXP);
-		map.put(MAX_TIME_BEFORE_IDLE, MAX_TIME_BEFORE_IDLE_EXP);
 		map.put(TARGET_ALPHA, TARGET_ALPHA_EXP);
 		map.put(TARGET_BETA, TARGET_BETA_EXP);
-		map.put(CELL_SIZE, CELL_SIZE_EXP);
 		map.put(ZONAL_DEMAND_AGGREGATOR_TYPE, ZONAL_DEMAND_AGGREGATOR_TYPE_EXP);
-		map.put(REBALANCING_ZONES_GENERATION, REBALANCING_ZONES_GENERATION_EXP);
-		map.put(REBALANCING_ZONES_SHAPE_FILE, REBALANCING_ZONES_SHAPE_FILE_EXP);
 		return map;
-	}
-
-	/**
-	 * @return -- {@value #INTERVAL_EXP}
-	 */
-	@StringGetter(INTERVAL)
-	public int getInterval() {
-		return interval;
-	}
-
-	/**
-	 * @param interval -- {@value #INTERVAL_EXP}
-	 */
-	@StringSetter(INTERVAL)
-	public void setInterval(int interval) {
-		this.interval = interval;
-	}
-
-	/**
-	 * @return -- {@value #MIN_SERVICE_TIME_EXP}
-	 */
-	@StringGetter(MIN_SERVICE_TIME)
-	public double getMinServiceTime() {
-		return minServiceTime;
-	}
-
-	/**
-	 * @param minServiceTime -- {@value #MIN_SERVICE_TIME_EXP}
-	 */
-	@StringSetter(MIN_SERVICE_TIME)
-	public void setMinServiceTime(double minServiceTime) {
-		this.minServiceTime = minServiceTime;
-	}
-
-	/**
-	 * @return -- {@value #MAX_TIME_BEFORE_IDLE_EXP}
-	 */
-	@StringGetter(MAX_TIME_BEFORE_IDLE)
-	public double getMaxTimeBeforeIdle() {
-		return maxTimeBeforeIdle;
-	}
-
-	/**
-	 * @param maxTimeBeforeIdle-- {@value #MAX_TIME_BEFORE_IDLE_EXP}
-	 */
-	@StringSetter(MAX_TIME_BEFORE_IDLE)
-	public void setMaxTimeBeforeIdle(double maxTimeBeforeIdle) {
-		this.maxTimeBeforeIdle = maxTimeBeforeIdle;
 	}
 
 	/**
@@ -229,22 +107,6 @@ public final class MinCostFlowRebalancingParams extends ReflectiveConfigGroup {
 	}
 
 	/**
-	 * @return -- {@value #CELL_SIZE_EXP}
-	 */
-	@StringGetter(CELL_SIZE)
-	public double getCellSize() {
-		return cellSize;
-	}
-
-	/**
-	 * @param cellSize -- {@value #CELL_SIZE_EXP}
-	 */
-	@StringSetter(CELL_SIZE)
-	public void setCellSize(double cellSize) {
-		this.cellSize = cellSize;
-	}
-
-	/**
 	 * @return -- {@value #ZONAL_DEMAND_AGGREGATOR_TYPE_EXP}
 	 */
 	@StringGetter(ZONAL_DEMAND_AGGREGATOR_TYPE)
@@ -256,41 +118,7 @@ public final class MinCostFlowRebalancingParams extends ReflectiveConfigGroup {
 	 * @param aggregatorType -- {@value #ZONAL_DEMAND_AGGREGATOR_TYPE_EXP}
 	 */
 	@StringSetter(ZONAL_DEMAND_AGGREGATOR_TYPE)
-	public void setZonalDemandAggregatorType(ZonalDemandAggregatorType aggregatorType) { this.zonalDemandAggregatorType = aggregatorType; }
-
-	/**
-	 * @return -- {@value #REBALANCING_ZONES_GENERATION_EXP}
-	 */
-	@StringGetter(REBALANCING_ZONES_GENERATION)
-	public RebalancingZoneGeneration getRebalancingZonesGeneration() {
-		return rebalancingZonesGeneration;
+	public void setZonalDemandAggregatorType(ZonalDemandAggregatorType aggregatorType) {
+		this.zonalDemandAggregatorType = aggregatorType;
 	}
-
-	/**
-	 * @param rebalancingZonesGeneration -- {@value #REBALANCING_ZONES_GENERATION_EXP}
-	 */
-	@StringSetter(REBALANCING_ZONES_GENERATION)
-	public void setRebalancingZonesGeneration(RebalancingZoneGeneration rebalancingZonesGeneration) { this.rebalancingZonesGeneration = rebalancingZonesGeneration; }
-
-	/**
-	 * @return {@link #REBALANCING_ZONES_SHAPE_FILE_EXP}
-	 */
-	@StringGetter(REBALANCING_ZONES_SHAPE_FILE)
-	public String getRebalancingZonesShapeFile() {
-		return rebalancingZonesShapeFile;
-	}
-
-	public URL getRebalancingZonesShapeFileURL(URL context) {
-		return ConfigGroup.getInputFileURL(context, rebalancingZonesShapeFile);
-	}
-
-	/**
-	 * @param rebalancingZonesShapeFile -- {@link #REBALANCING_ZONES_SHAPE_FILE_EXP}
-	 */
-	@StringSetter(REBALANCING_ZONES_SHAPE_FILE)
-	public MinCostFlowRebalancingParams setRebalancingZonesShapeFile(String rebalancingZonesShapeFile) {
-		this.rebalancingZonesShapeFile = rebalancingZonesShapeFile;
-		return this;
-	}
-
 }
